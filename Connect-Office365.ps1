7c1bd30ee1d0c6a3f2f4b451c78b220a0c3cca9e
waelzemzem <wael.zemzem@outlook.fr>
2024-01-23 11:48:44 +0100

    Add files via upload

---
+++ Connect-Office365.ps1
@@ -0,0 +1,5 @@
+﻿$cred = Get-Credential
+[System.Net.WebRequest]::DefaultWebProxy.Credentials = $cred
+Connect-SPOService -Url https://mocca365-admin.sharepoint.com -Credential wael@mocca365.onmicrosoft.com
+
+Connect-PnPOnline  -Url https://mocca365.sharepoint.com/sites/com-wz -Credential wael@mocca365.onmicrosoft.com
